897fba55af5578938dfd211463ed788bddb534ed
SUNEETHA <suneetha.ila@gmail.com>
2025-01-01 13:11:11 +0530

    intial

---
+++ src/test/java/org/example/TestExample.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestExample {
+
+    @Test
+    void test(){
+        Assert.assertTrue(true);
+    }
+}
